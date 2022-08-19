cd4704b990051eb32fad70b816326ecfd151bee9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-19 23:42:28 +0530

    pass data down to children

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input() title: string | undefined;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
